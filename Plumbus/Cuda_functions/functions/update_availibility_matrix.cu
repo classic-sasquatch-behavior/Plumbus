7ffd3e3520e5456c3fe0171c0729fa1e1d0f5432
classic-sasquatch-behavior <classic.sasquatch.behavior@gmail.com>
2022-05-18 19:33:04 -0400

    rewrote update availibility matrix to not be slow as molasses

--- Plumbus/Cuda_functions/functions/update_availibility_matrix.cu
+++ Plumbus/Cuda_functions/functions/update_availibility_matrix.cu
@@ -6,49 +6,74 @@
 
 
 
+__global__ void availibility_matrix_sum_columns_kernel(cv::cuda::PtrStepSzf responsibility_matrix, cv::cuda::PtrStepSzf column_sums, int N) {
+
+	int col = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+	if (col >= N) { return; }
 
 
-__global__ void update_availibility_matrix_kernel(cv::cuda::PtrStepSzf responsibility_matrix, cv::cuda::PtrStepSzf availibility_matrix, int N) {
+	int column_sum = 0;
+	for (int row = 0; row < N; row++) {
+		int row_val = responsibility_matrix(row, col);
+		if (row_val > 0) {
+			column_sum += row_val;
+		}
+	}
 
+	column_sums(0, col) = column_sum;
+}
+
+
+__global__ void availibility_matrix_calculate_off_diagonal(cv::cuda::PtrStepSzf responsibility_matrix, cv::cuda::PtrStepSzf column_sums, cv::cuda::PtrStepSzf availibility_matrix, int N) {
+	
 	int row = (blockIdx.y * blockDim.y) + threadIdx.y;
 	int col = (blockIdx.x * blockDim.x) + threadIdx.x;
-	if (row >= N || col >= N) { return; }
+	if (row >= N || col >= N || row == col) { return; }
+	
 
+	int responsibility_at_pair = responsibility_matrix(row, col);
+	int self_responsibility = responsibility_matrix(col, col);
+	int responsibility_col_sum = column_sums(0,col);
+	int result = 0;
 
-	float result = 0;
-
-	if (row == col) { //diagonal case
-
-		float column_sum_of_positives = 0;
-		for (int row_traversal = 0; row_traversal < N; row_traversal++) {
-			if (row_traversal != row) {
-				float val_at_cell = responsibility_matrix(row_traversal, col);
-				if (val_at_cell > 0) {
-					column_sum_of_positives += val_at_cell;
-				}
-			}
-		}
-		result = column_sum_of_positives;
+	if (responsibility_at_pair > 0) {
+		responsibility_col_sum -= responsibility_at_pair;
 	}
-	else if (row != col){ //off-diagonal case
-		float column_self_responsibility = responsibility_matrix(col, col);
-		float column_sum_of_positives = column_self_responsibility;
+	if (self_responsibility > 0) {
+		responsibility_col_sum -= self_responsibility;
+	}
 
-		for (int col_traversal = 0; col_traversal < N; col_traversal++) {
-			if (col_traversal != row && col_traversal != col) {
-				float val_at_cell = responsibility_matrix(col_traversal, col);
-				if (val_at_cell > 0) {
-					column_sum_of_positives += val_at_cell;
-				}
-			}
-		}
-		if (column_sum_of_positives < 0) {
-			result = column_sum_of_positives;
-		}
+	result = responsibility_col_sum + self_responsibility;
+	if (result > 0) {
+		result = 0;
 	}
 
 	availibility_matrix(row, col) = result;
 }
+
+
+__global__ void availibility_matrix_calculate_diagonal(cv::cuda::PtrStepSzf responsibility_matrix, cv::cuda::PtrStepSzf column_sums, cv::cuda::PtrStepSzf availibility_matrix, int N) {
+	int diag = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+	if (diag >= N) { return; }
+
+	int self_responsibility = responsibility_matrix(diag, diag);
+	int responsibility_col_sum = column_sums(0, diag);
+	
+	if (self_responsibility > 0) {
+		responsibility_col_sum -= self_responsibility;
+	}
+
+	availibility_matrix(diag, diag) = responsibility_col_sum;
+}
+
+
+
+
+
+
+
 
 
 
@@ -57,14 +82,77 @@ __global__ void update_availibility_matrix_kernel(cv::cuda::PtrStepSzf responsib
 void update_availibility_matrix_launch(cv::cuda::GpuMat& responsibility_matrix, cv::cuda::GpuMat& availibity_matrix, int N) {
 
 
+	//sum columns prep
+	
+	unsigned int sum_columns_block_dim_x = 1024;
+	unsigned int sum_columns_grid_dim_x = ((N - (N % 1024)) / 1024) + 1;
 
-	unsigned int grid_dim_xy = ((N - (N % 32)) / 32) + 1;
-	unsigned int block_dim_xy = 32;
+	dim3 sum_columns_num_blocks = {sum_columns_grid_dim_x,1,1};
+	dim3 sum_columns_threads_per_block = {sum_columns_block_dim_x,1,1};
 
-	dim3 num_blocks = { grid_dim_xy,grid_dim_xy,1 };
-	dim3 threads_per_block = { block_dim_xy,block_dim_xy,1 };
+	cv::cuda::GpuMat column_sums(cv::Size(N, 1), responsibility_matrix.type());
+
+
+	//off-diagonal prep
+
+	unsigned int calculate_off_diagonal_grid_dim_xy = ((N - (N % 32)) / 32) + 1;
+	unsigned int calculate_off_diagonal_block_dim_xy = 32;
+
+	dim3 calculate_off_diagonal_num_blocks = { calculate_off_diagonal_grid_dim_xy,calculate_off_diagonal_grid_dim_xy,1 };
+	dim3 calculate_off_diagonal_threads_per_block = { calculate_off_diagonal_block_dim_xy,calculate_off_diagonal_block_dim_xy,1 };
+
+
+	//on-diagonal prep
+
+	unsigned int calculate_on_diagonal_grid_dim_x = 1024;
+	unsigned int calculate_on_diagonal_block_dim_x = ((N - (N % 1024)) / 1024) + 1;
+	dim3 calculate_on_diagonal_num_blocks = { calculate_on_diagonal_grid_dim_x ,1,1 };
+	dim3 calculate_on_diagonal_threads_per_block = { calculate_on_diagonal_block_dim_x,1,1 };
+
+
+	availibility_matrix_sum_columns_kernel << <sum_columns_num_blocks, sum_columns_threads_per_block >> > (responsibility_matrix, column_sums, N);
+	cudaDeviceSynchronize();
+
+	availibility_matrix_calculate_off_diagonal << <calculate_off_diagonal_num_blocks, calculate_off_diagonal_threads_per_block >> > (responsibility_matrix, column_sums, availibity_matrix, N);
+	availibility_matrix_calculate_diagonal << <calculate_on_diagonal_num_blocks, calculate_on_diagonal_threads_per_block >> > (responsibility_matrix, column_sums, availibity_matrix, N);
+	cudaDeviceSynchronize();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	update_availibility_matrix_kernel << <num_blocks, threads_per_block >> > (responsibility_matrix, availibity_matrix, N);
+	//launch kernels
 	cudaDeviceSynchronize();
+
+
+
+
+
+
+
+
+
+
 }
